c6c268505456322bb3d9eb575c9b3f27da1204a8
Barbara <barbarareyes289@gmail.com>
2020-01-16 00:32:22 -0300

    login

--- login/zafary.js
+++ login/zafary.js
@@ -11,10 +11,22 @@ function mostrarDatos() {
     option.value = '';
     option2.value = "";
     option.focus();
-    if (textValue1 === 'barbarareyes289@gmail.com' && textValue2 === 'barbara') {
-        console.log('Permiso concedido para ingresar al sistema');
+    if (textValue1 === '' || textValue2 === '') {
+        console.log('validación de vacío');
+        document.getElementById("error").innerHTML = " Debe rellenar todos los datos ";
     } else {
-
-        console.log('Permiso denegado- credenciales incorrectas');
+        console.log('paso el filtro');
+        if (textValue1 === 'ejemplo@gmail.com' && textValue2 === '123456') {
+            console.log('Permiso concedido para ingresar al sistema');
+            document.getElementById("error").innerHTML = " ";
+            const boton = document.querySelector('#button');
+            boton.addEventListener('click', funcionQueRedirecciona);
+            function funcionQueRedirecciona() {
+            document.location.assign('home/Home.html');
+            }
+        } else {
+            console.log('Permiso denegado- credenciales incorrectas');
+            document.getElementById("error").innerHTML = "Nombre de usuario y/o contraseña incorrectos";
+        }
     }
 }
